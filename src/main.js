a580825153aedc58f60e55d361e2a0faeb8750ec
Alex Curran <alexcurran.va@gmail.com>
2025-07-09 16:20:37 -0500

    Date/time with timezone

--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ Date.prototype.toTemporalInstant = toTemporalInstant;
 // current date and time
 const myDateWithTime = Temporal.Now.plainDateTimeISO();
 console.log(myDateWithTime.toString()); // ISO 8601 date format
+
+const myDateWithTimeZone = Temporal.Now.zonedDateTimeISO();
+console.log(myDateWithTimeZone.toString());
