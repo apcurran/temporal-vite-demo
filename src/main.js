e849ee12526fe7dc010360179fbca3ae24b67596
Alex Curran <alexcurran.va@gmail.com>
2025-07-09 16:17:10 -0500

    Basic date and time format in ISO 8601

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import { Temporal, Intl, toTemporalInstant } from "@js-temporal/polyfill";
 Date.prototype.toTemporalInstant = toTemporalInstant;
+
+// current date and time
+const myDateWithTime = Temporal.Now.plainDateTimeISO();
+console.log(myDateWithTime.toString()); // ISO 8601 date format
